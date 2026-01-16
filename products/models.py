4be4d72c62b654379237756c0840fc680874f4ac
Matheus Fabião <matheusfabiao.dev@gmail.com>
2026-01-16 19:00:20 -0300

    feat: new product price validator added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from companies.models import Company
@@ -9,7 +10,13 @@ class Product(models.Model):
     sku = models.CharField(
         max_length=50, unique=True, help_text="Código interno do produto"
     )
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(
+        validators=[
+            MinValueValidator(0.00, "O preço não pode ser menor que 0.00."),
+        ],
+        max_digits=10,
+        decimal_places=2,
+    )
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
